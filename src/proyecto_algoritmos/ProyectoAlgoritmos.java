ac77719ce5cfe9f0570c5a2f6b3f56ba2d877c7d
GonzaLangone <gojav@hotmail.com>
2023-04-26 23:16:28 -0300

    probando cambiarlo

--- src/proyecto_algoritmos/ProyectoAlgoritmos.java
+++ src/proyecto_algoritmos/ProyectoAlgoritmos.java
@@ -5,6 +5,7 @@ public class ProyectoAlgoritmos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Gonza cambiando texto intento 100 ");
+		System.out.print("PROBANDO");
 	}
 
 }
